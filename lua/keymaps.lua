b19cb3011cdce4ac5799ebf3db9009fd9e0064e1
Jonathan Struebel <jonathan.struebel@gmail.com>
2025-12-29 21:16:15 -0700

    updated keymaps and added descriptions to all

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,65 +1,84 @@
 -- New Key maps
+local map = vim.keymap.set
 
 -- Yank to end of line
-vim.keymap.set("n", "Y", "yg$", {})
+map("n", "Y", "yg$", {desc="Yenk text from cursor to end of line"})
 
 -- Keep selection when indenting/outdenting
-vim.keymap.set("v", "<", "<gv", {noremap=true, silent=true})
-vim.keymap.set("v", ">", ">gv", {noremap=true, silent=true})
-vim.keymap.set("x", "<", "<gv", {noremap=true, silent=true})
-vim.keymap.set("x", ">", ">gv", {noremap=true, silent=true})
+map("v", "<", "<gv", {noremap=true, silent=true, desc="Shift indenting one level left"})
+map("v", ">", ">gv", {noremap=true, silent=true, desc="Shift indenting one level right"})
+map("x", "<", "<gv", {noremap=true, silent=true, desc="Shift indenting one level left"})
+map("x", ">", ">gv", {noremap=true, silent=true, desc="Shift indenting one level right"})
 
--- Clear search highlights
-vim.keymap.set("n", "<leader>c", ":nohl<CR>", {noremap=true, silent=true})
+-- Search
+map("n", "n", "nzzzv", {noremap=true, silent=true, desc="Goto next result"})
+map("n", "N", "Nzzzv", {noremap=true, silent=true, desc="Goto previous result"})
+map("n", "<leader>c", ":nohl<CR>", {noremap=true, silent=true, desc="Clear search highlighting"})
+
+-- Replace
+map("n", "<leader>rl", ":s/\\v", {noremap=true, silent=true, desc="Replace in current line"})
+map("n", "<leader>rf", ":%s/\\v", {noremap=true, silent=true, desc="Replace in current file"})
+map("n", "<leader>rv", ":%s/\\%V", {noremap=true, silent=true, desc="Replace in current visual selection"})
+
+-- Selections
+map("n", "L", "vg_", {noremap=true, silent=true, desc="Select text from cursor to end of line"})
+map("n", "<leader>sa", "ggVG", {noremap=true, silent=true, desc="Select all text"})
 
 -- Dump helpful information to a new buffer
 -- Reference: https://tech.serhatteker.com/post/2022-07/dump-command-output-to-buffer-in-neovim/
-vim.keymap.set("n", "<leader>?d", "<cmd>new|pu=execute('digraphs')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>?k", "<cmd>new|pu=execute('map')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>?kn", "<cmd>new|pu=execute('nmap')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>?ki", "<cmd>new|pu=execute('imap')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>?kv", "<cmd>new|pu=execute('vmap')<CR>", {noremap=true, silent=true})
+map("n", "<leader>?d", "<cmd>new|pu=execute('digraphs')<CR>", {noremap=true, silent=true, desc="Dump all digraphs to new buffer"})
+map("n", "<leader>?k", "<cmd>new|pu=execute('map')<CR>", {noremap=true, silent=true, desc="Dump all keymaps to new buffer"})
+map("n", "<leader>?kn", "<cmd>new|pu=execute('nmap')<CR>", {noremap=true, silent=true,desc="Dump normal keymaps to new buffer"})
+map("n", "<leader>?ki", "<cmd>new|pu=execute('imap')<CR>", {noremap=true, silent=true,desc="Dump insert keymaps to new buffer"})
+map("n", "<leader>?kv", "<cmd>new|pu=execute('vmap')<CR>", {noremap=true, silent=true,desc="Dump visual keymaps to new buffer"})
 
 if not vim.g.vscode then
     -- Move between windows/splits
-    vim.keymap.set("n", "<C-h>", "<C-w>h", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-j>", "<C-w>j", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-k>", "<C-w>k", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-l>", "<C-w>l", {noremap=true, silent=true})
+    map("n", "<C-h>", "<C-w>h", {noremap=true, silent=true,desc="Move to window on left of current"})
+    map("n", "<C-j>", "<C-w>j", {noremap=true, silent=true,desc="Move to window below current"})
+    map("n", "<C-k>", "<C-w>k", {noremap=true, silent=true,desc="Move to window above current"})
+    map("n", "<C-l>", "<C-w>l", {noremap=true, silent=true,desc="Move to window on right of current"})
 
     -- Move between windows/splits from terminal
-    vim.keymap.set("t", "<C-h>", "<cmd>wincmd h", {silent=true})
-    vim.keymap.set("t", "<C-j>", "<cmd>wincmd j", {silent=true})
-    vim.keymap.set("t", "<C-k>", "<cmd>wincmd k", {silent=true})
-    vim.keymap.set("t", "<C-l>", "<cmd>wincmd l", {silent=true})
+    map("t", "<C-h>", "<cmd>wincmd h", {silent=true,desc="Move to window on left of current"})
+    map("t", "<C-j>", "<cmd>wincmd j", {silent=true,desc="Move to window below current"})
+    map("t", "<C-k>", "<cmd>wincmd k", {silent=true,desc="Move to window above current"})
+    map("t", "<C-l>", "<cmd>wincmd l", {silent=true,desc="Move to window on right of current"})
 
     -- Resize windows using arrow keys
-    vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-down>", ":resize +2<CR>", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-right>", ":vertical resize -2<CR>", {noremap=true, silent=true})
-    vim.keymap.set("n", "<C-left>", ":vertical resize +2<CR>", {noremap=true, silent=true})
+    map("n", "<C-Up>", ":resize -2<CR>", {noremap=true, silent=true,desc="Resize window up"})
+    map("n", "<C-down>", ":resize +2<CR>", {noremap=true, silent=true,desc="Resize window down"})
+    map("n", "<C-right>", ":vertical resize -2<CR>", {noremap=true, silent=true,desc="Resize window to right"})
+    map("n", "<C-left>", ":vertical resize +2<CR>", {noremap=true, silent=true,desc="Resize window to left"})
 
     -- Manage Window Splits
-    vim.keymap.set("n", "<leader>sv", "<C-w>v", {silent=true})
-    vim.keymap.set("n", "<leader>sh", "<C-w>s", {silent=true})
-    vim.keymap.set("n", "<leader>se", "<C-w>=", {silent=true})
+    map("n", "<leader>wv", "<C-w>v", {silent=true,desc="Open new window to right (vertical)"})
+    map("n", "<leader>wh", "<C-w>s", {silent=true,desc="Open new window below (horizontal)"})
+    map("n", "<leader>we", "<C-w>=", {silent=true,desc="Make windows equal"})
+    map("n", "<leader>wr", "<C-w>r", {silent=true,desc="Move window to the right/down"})
+    map("n", "<leader>wR", "<C-w>R", {silent=true,desc="Move window to the left/up"})
+    map("n", "<leader>wH", "<C-w>H", {silent=true,desc="Move window to the left, full height"})
+    map("n", "<leader>wJ", "<C-w>J", {silent=true,desc="Move window to the bottom, full width"})
+    map("n", "<leader>wK", "<C-w>K", {silent=true,desc="Move window to the top, full width"})
+    map("n", "<leader>wL", "<C-w>L", {silent=true,desc="Move window to the right, full height"})
     
     -- Manage Tabs
-    vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {silent=true})
-    vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {silent=true})
-    vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", {silent=true})
+    map("n", "<leader>tn", "<cmd>tabnew<CR>", {silent=true,desc="Open new tab"})
+    map("n", "<leader>tf", "<cmd>tabnew %<CR>", {silent=true,desc="Open new tab with current file"})
+    map("n", "<leader>tc", "<cmd>tabclose<CR>", {silent=true,desc="Close current tab"})
+    map("n", "<leader>to", "<cmd>tabonly<CR>", {silent=true,desc="Close all other tabs"})
 
     -- Navigate Buffers
-    vim.keymap.set("n", "<leader>b", "<cmd>buffers<CR>", {silent=true})
-    vim.keymap.set("n", "[b", "<cmd>bprevious<CR>", {silent=true})
-    vim.keymap.set("n", "]b", "<cmd>bnext<CR>", {silent=true})
+    map("n", "<leader>b", "<cmd>buffers<CR>", {silent=true,desc="Display all open buffers"})
+    map("n", "[b", "<cmd>bprevious<CR>", {silent=true,desc="Load previous buffer in current window"})
+    map("n", "]b", "<cmd>bnext<CR>", {silent=true,desc="Load next buffer in current window"})
     
     --Exit insert mode in terminal (default is confusing)
-    vim.keymap.set("t", "<C-[>", "<C-\\><C-N>", {noremap=true, silent=true})
+    map("t", "<C-[>", "<C-\\><C-N>", {noremap=true, silent=true,desc="Exit insert mode (terminal)"})
 
     -- Move lines in visual mode
-    vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", {noremap=true, silent=true})
-    vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", {noremap=true, silent=true})
+    map("x", "J", ":m '>+1<CR>gv=gv", {noremap=true, silent=true,desc="Move line up one (visual mode)"})
+    map("x", "K", ":m '<-2<CR>gv=gv", {noremap=true, silent=true,desc="Move line down one (visual mode)"})
 
     -- Reload configuration
     local function reload()
@@ -67,6 +86,6 @@ if not vim.g.vscode then
       print("Configuration reloaded")
     end
 
-    vim.keymap.set("n", "<leader>ve", ":tabnew $MYVIMRC<CR>", {noremap=true, silent=true})
-    vim.keymap.set("n", "<leader>vs", reload, {noremap=true, silent=true})
+    map("n", "<leader>ve", ":tabnew $MYVIMRC<CR>", {noremap=true, silent=true,desc="Open neovim config in new tab"})
+    map("n", "<leader>vs", reload, {noremap=true, silent=true,desc="Reload neovim config"})
 end
--- vscode/lua/keymaps.lua
+++ vscode/lua/keymaps.lua
@@ -1,4 +1,6 @@
 -- VSCode specific maps
+local map = vim.keymap.set
+
 vim.g.mapleader = " " -- use space
 vim.g.maplocalleader = " " -- use space
 
@@ -28,64 +30,64 @@ end
 local comment_block_line = function ()
     return comment_block() .. "_"
 end
-vim.keymap.set({"x", "n", "o" }, "gc", comment_line, { expr = true })
-vim.keymap.set("n", "gcc", comment_line_line, { expr = true })
-vim.keymap.set({"x", "n", "o" }, "gb", comment_block, { expr = true })
-vim.keymap.set("n", "gbc", comment_block_line, { expr = true })
+map({"x", "n", "o" }, "gc", comment_line, { expr = true, desc = "Comment with line style" })
+map("n", "gcc", comment_line_line, { expr = true, desc = "Comment with line style" })
+map({"x", "n", "o" }, "gb", comment_block, { expr = true, desc = "Comment with block style" })
+map("n", "gbc", comment_block_line, { expr = true, desc = "Comment with block style" })
 
 -- Code Navigation
-vim.keymap.set("n", "gr", "<cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "gd", "<cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "gy", "<cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "gi", "<cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>", {noremap=true, silent=true})
+map("n", "gr", "<cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", {noremap=true, silent=true, desc="Goto Reference"})
+map("n", "gd", "<cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>", {noremap=true, silent=true, desc="Goto Definition"})
+map("n", "gy", "<cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>", {noremap=true, silent=true, desc="Goto Type Definition"})
+map("n", "gi", "<cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>", {noremap=true, silent=true, desc="Goto Implementation"})
 
 -- Refactoring
-vim.keymap.set("n", "<leader>r", "<cmd>call VSCodeNotify('editor.action.rename')<CR>", {noremap=true, silent=true})
+map("n", "<leader>r", "<cmd>call VSCodeNotify('editor.action.rename')<CR>", {noremap=true, silent=true, desc="Rename"})
 
 -- Move between windows/splits
-vim.keymap.set("n", "<C-h>", "<cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<C-j>", "<cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<C-k>", "<cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<C-l>", "<cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>", {noremap=true, silent=true})
+map("n", "<C-h>", "<cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>", {noremap=true, silent=true,desc="Move to window on left of current"})
+map("n", "<C-j>", "<cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>", {noremap=true, silent=true,desc="Move to window below current"})
+map("n", "<C-k>", "<cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>", {noremap=true, silent=true,desc="Move to window above current"})
+map("n", "<C-l>", "<cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>", {noremap=true, silent=true,desc="Move to window on right of current"})
 
 -- Manage Window Splits
-vim.keymap.set("n", "<leader>sv", "<cmd>call VSCodeNotify('workbench.action.splitEditorRight')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>sh", "<cmd>call VSCodeNotify('workbench.action.splitEditorDown')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>se", "<C-w>=", {silent=true})
+map("n", "<leader>wv", "<cmd>call VSCodeNotify('workbench.action.splitEditorRight')<CR>", {silent=true,desc="Open new window to right (vertical)"})
+map("n", "<leader>wh", "<cmd>call VSCodeNotify('workbench.action.splitEditorDown')<CR>", {silent=true,desc="Open new window below (horizontal)"})
+map("n", "<leader>we", "<C-w>=", {silent=true,desc="Make windows equal"})
 
 -- Manage Tabs
-vim.keymap.set("n", "<leader>to", "<cmd>call VSCodeNotify('workbench.action.files.newUntitledFile')<CR>", {silent=true})
+map("n", "<leader>tn", "<cmd>call VSCodeNotify('workbench.action.files.newUntitledFile')<CR>", {silent=true,desc="Open new tab"})
 
 -- Move lines in visual mode
-vim.keymap.set("x", "J", ":m '>+1<CR>gv", {noremap=true, silent=true})
-vim.keymap.set("x", "K", ":m '<-2<CR>gv", {noremap=true, silent=true})
+map("x", "J", ":m '>+1<CR>gv", {noremap=true, silent=true,desc="Move line up one (visual mode)"})
+map("x", "K", ":m '<-2<CR>gv", {noremap=true, silent=true,desc="Move line down one (visual mode)"})
 
 -- Toggle folding
-vim.keymap.set("n", "za", "<cmd>call VSCodeNotify('editor.toggleFold')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "zR", "<cmd>call VSCodeNotify('editor.unfoldAll')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "zM", "<cmd>call VSCodeNotify('editor.foldAll')<CR>", {noremap=true, silent=true})
+map("n", "za", "<cmd>call VSCodeNotify('editor.toggleFold')<CR>", {noremap=true, silent=true, desc="Toggle current fold"})
+map("n", "zR", "<cmd>call VSCodeNotify('editor.unfoldAll')<CR>", {noremap=true, silent=true, desc="Open all folds"})
+map("n", "zM", "<cmd>call VSCodeNotify('editor.foldAll')<CR>", {noremap=true, silent=true, desc="Close all folds"})
 
 -- Toggle panes/open explorer & find panes
-vim.keymap.set("n", "<leader>s", "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>m", "<cmd>call VSCodeNotify('editor.action.toggleMinimap')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>o", "<cmd>call VSCodeNotify('workbench.action.output.toggleOutput')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>p", "<cmd>call VSCodeNotify('workbench.action.togglePanel')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>t", "<cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>d", "<cmd>call VSCodeNotify('editor.action.showHover')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>a", "<cmd>call VSCodeNotify('editor.action.quickFix')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>g", "<cmd>call VSCodeNotify('workbench.view.scm')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>bp", "<cmd>call VSCodeNotify('editor.debug.action.toggleBreakpoint')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>sp", "<cmd>call VSCodeNotify('workbench.actions.view.problems')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>cn", "<cmd>call VSCodeNotify('notifications.clearAll')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>e", "<cmd>call VSCodeNotify('workbench.view.explorer')<CR>", {silent=true})
-vim.keymap.set("n", "<leader>ff", "<cmd>Ex<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>fg", "<cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", {noremap=true, silent=true})
+map("n", "<leader>s", "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>", {silent=true,desc="Toggle sidebar"})
+map("n", "<leader>m", "<cmd>call VSCodeNotify('editor.action.toggleMinimap')<CR>", {silent=true,desc="Toggle minimap"})
+map("n", "<leader>o", "<cmd>call VSCodeNotify('workbench.action.output.toggleOutput')<CR>", {silent=true,desc="Toggle output panel"})
+map("n", "<leader>p", "<cmd>call VSCodeNotify('workbench.action.togglePanel')<CR>", {silent=true,desc="Toggle panel"})
+map("n", "<leader>t", "<cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", {silent=true,desc="Toggle terminal panel"})
+map("n", "<leader>d", "<cmd>call VSCodeNotify('editor.action.showHover')<CR>", {silent=true,desc="Show hover tooltip"})
+map("n", "<leader>a", "<cmd>call VSCodeNotify('editor.action.quickFix')<CR>", {silent=true,desc="Open quickfix"})
+map("n", "<leader>g", "<cmd>call VSCodeNotify('workbench.view.scm')<CR>", {silent=true,desc="View SCM"})
+map("n", "<leader>bp", "<cmd>call VSCodeNotify('editor.debug.action.toggleBreakpoint')<CR>", {silent=true,desc="Toggle breakpoint"})
+map("n", "<leader>sp", "<cmd>call VSCodeNotify('workbench.actions.view.problems')<CR>", {silent=true,desc="Show problems"})
+map("n", "<leader>cn", "<cmd>call VSCodeNotify('notifications.clearAll')<CR>", {silent=true,desc="Clear notifications"})
+map("n", "<leader>e", "<cmd>call VSCodeNotify('workbench.view.explorer')<CR>", {silent=true,desc="Open explorer sidebar"})
+map("n", "<leader>ff", "<cmd>Ex<CR>", {noremap=true, silent=true,desc="Find File"})
+map("n", "<leader>fg", "<cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", {noremap=true, silent=true, desc="Find in all files"})
 
 -- Perforce Extension
-vim.keymap.set("n", "<leader>pc", "<cmd>call VSCodeNotify('perforce.menuFunctions')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>pa", "<cmd>call VSCodeNotify('perforce.add')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>pe", "<cmd>call VSCodeNotify('perforce.edit')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>pd", "<cmd>call VSCodeNotify('perforce.diff')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>pm", "<cmd>call VSCodeNotify('perforce.move')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>pr", "<cmd>call VSCodeNotify('perforce.revert')<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>ps", "<cmd>call VSCodeNotify('perforce.sync')<CR>", {noremap=true, silent=true})
+map("n", "<leader>pc", "<cmd>call VSCodeNotify('perforce.menuFunctions')<CR>", {noremap=true, silent=true, desc="Perforce commands"})
+map("n", "<leader>pa", "<cmd>call VSCodeNotify('perforce.add')<CR>", {noremap=true, silent=true, desc="Perforce add file"})
+map("n", "<leader>pe", "<cmd>call VSCodeNotify('perforce.edit')<CR>", {noremap=true, silent=true, desc="Perforce open for edit"})
+map("n", "<leader>pd", "<cmd>call VSCodeNotify('perforce.diff')<CR>", {noremap=true, silent=true, desc="Perforce show diffs"})
+map("n", "<leader>pm", "<cmd>call VSCodeNotify('perforce.move')<CR>", {noremap=true, silent=true, desc="Perforce move file"})
+map("n", "<leader>pr", "<cmd>call VSCodeNotify('perforce.revert')<CR>", {noremap=true, silent=true, desc="Perforce revert changes"})
+map("n", "<leader>ps", "<cmd>call VSCodeNotify('perforce.sync')<CR>", {noremap=true, silent=true, desc="Perforce sync to server"})
