82a91e52429df995eb2698432a1589935e42c032
Jonathan Struebel <jonathan.struebel@gmail.com>
2026-01-11 08:48:19 -0700

    Fixed OS detection logic to be boolean

--- init.lua
+++ init.lua
@@ -11,8 +11,8 @@ vim.g.maplocalleader = " " -- use space
 
 if not vim.g.vscode then
     -- set variables to control configuration
-    vim.g.windows = vim.fn.has("win32") or vim.fn.has("win64")
-    vim.g.macos = vim.fn.has("mac")
+    vim.g.windows = (vim.fn.has("win32") or vim.fn.has("win64")) == 1
+    vim.g.macos = vim.fn.has("mac") == 1
     vim.g.is_usb = os.getenv("NEOVIM_USB")
     vim.g.is_ide = os.getenv("NEOVIM_IDE")
     if vim.g.windows then
